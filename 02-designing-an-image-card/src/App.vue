b9e9708b6dc5efb3621e856bf8839046a4b344ac
iarmankhan <armankhan9244@gmail.com>
2020-02-05 23:52:13 +0530

    Designing Text Content

--- 02-designing-an-image-card/src/App.vue
+++ 02-designing-an-image-card/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="antialiased text-gray-900">
     <div class="bg-gray-200 min-h-screen p-8 flex items-center justify-center">
       <PropertyCard :property="property" />
     </div>
@@ -7,27 +7,28 @@
 </template>
 
 <script>
-import PropertyCard from './components/PropertyCard.vue'
+import PropertyCard from "./components/PropertyCard.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     PropertyCard
   },
   data: () => ({
     property: {
-      imageUrl: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      imageAlt: 'Rear view of modern home with pool',
+      imageUrl:
+        "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+      imageAlt: "Rear view of modern home with pool",
       beds: 3,
       baths: 2,
-      title: "Modern home in city center",
+      title: "Modern home in city center Modern home in city center Modern home in city center",
       priceInCents: 190000,
       formattedPrice: "$1,900.00",
       reviewCount: 34,
-      rating: 4,
+      rating: 4
     }
   })
-}
+};
 </script>
 
 <style src="./assets/tailwind.css"></style>
