846b9372ef8636715e5e9127b996679d680f62ec
iarmankhan <armankhan9244@gmail.com>
2020-02-06 00:47:34 +0530

    Locking image to a fixed aspect ratio

--- 02-designing-an-image-card/tailwind.config.js
+++ 02-designing-an-image-card/tailwind.config.js
@@ -1,16 +1,13 @@
 module.exports = {
   theme: {
     extend: {
-      colors:{
-        'brand-blue' : '#1992d4'
+      spacing: {
+        '1/2': '50%',
+
+        '2/3': '66.666667%',
       },
-      spacing:{
-        '72': '18rem'
-      }
     }
   },
-  variants: {
-    backgroundColor: ['responsive', 'hover', 'focus', 'active']
-  },
+  variants: {},
   plugins: []
 }
