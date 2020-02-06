c282b9f2694bf22cb374f50cb42a76ebfae1f8ab
iarmankhan <armankhan9244@gmail.com>
2020-02-06 21:07:38 +0530

    Responsive navbar initial commit

---
+++ 03-a-responsive-navbar/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <NavBar />
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style src="./assets/tailwind.css"></style>
+
