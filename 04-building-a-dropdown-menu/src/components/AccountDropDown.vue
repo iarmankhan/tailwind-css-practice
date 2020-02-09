f707ba65514678c7266110b5cb96ec465395d202
iarmankhan <armankhan9244@gmail.com>
2020-02-09 16:13:25 +0530

    Making the dropdown interactive

--- 04-building-a-dropdown-menu/src/components/AccountDropDown.vue
+++ 04-building-a-dropdown-menu/src/components/AccountDropDown.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="relative">
-        <button class="block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white">
+        <button @click="isOpen = !isOpen" class="relative z-10 block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white">
             <img class="h-full w-full object-cover" src="http://demo.powowbox.com/powowbox/avatar_demo/Jane_0001.png" alt="Your Avatar">
         </button>
-        <div class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
+        <button v-if="isOpen" @click="isOpen = false" tabindex="-1" class="fixed inset-0 h-full w-full bg-black opacity-50 cursor-default"></button>
+        <div v-if="isOpen" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
             <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Account Settings</a>
             <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Support</a>
             <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Sign Out</a>
@@ -13,6 +14,23 @@
 
 <script>
 export default {
-    
+    data() {
+        return {
+            isOpen: false
+        }
+    },
+    created() {
+        const handleEscape = (e) => {
+            if(e.key === 'Esc' || e.key === 'Escape'){
+                this.isOpen = false
+            }
+        }
+
+        document.addEventListener('keydown', handleEscape)
+
+        this.$once('hook:beforeDestroy', () => {
+            document.removeEventListener('keydown', handleEscape)
+        })
+    },
 }
 </script>
